33dd1aa1cd0907322095cace8a63f10b68f4b77f
s86110200 <s86110200@gmail.com>
2012-12-17 19:01:03 +0800

    Main_Process二次修改，增加產品ID搜尋所處文章功能

--- src/Main_Process.java
+++ src/Main_Process.java
@@ -21,7 +21,6 @@ public class Main_Process {
         String str_readLine = "";
 		StringBuilder readStr = new StringBuilder("");
 		String[] json_product_ID_content = new String[50];
-		int flag;
 		frj = new FileReader("myProduct.json");
         brj = new BufferedReader(frj);
         
@@ -52,26 +51,11 @@ public class Main_Process {
 			//System.out.println(j_a.length());
 			/*目前可取得產品ID以及做比對*/
 			for(int i=0;i<j_a.length();i++)	{
-			//for(int i=0;i<15;i++)	{
 				j_product_ID = j_a.get(i);
 				j_product_name = j_ob.getJSONObject("Product").getJSONArray(j_product_ID.toString()).get(0);
 				
 				for(int j=0;j<j_product_name.toString().split(" ").length;j++)
 					json_product_ID_content[j] = j_product_name.toString().split(" ")[j];
-				
-				//System.out.println(j_a.get(i));
-				//System.out.println(j_product_name.toString().split(" ").length);
-				
-				/*
-				for(int j=0;j<j_product_name.toString().split(" ").length;j++) {
-					flag = json_product_ID_content[j].toLowerCase().compareTo("RAMD600".toLowerCase());
-				
-					if(flag == 0)
-						System.out.println("a"+i);
-					else
-						System.out.println("b"+j);
-				}
-				*/
 			}
 		} catch (JSONException e1) {
 			// TODO 自動產生的 catch 區塊
@@ -81,53 +65,52 @@ public class Main_Process {
 		return j_a;
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public String[] result_search(String search_str) throws IOException {
 		/*result 用*/
 		FileReader fr = null;
         BufferedReader br = null;
-        File file = new File("result_.csv");
-		FileWriter fw;
-		String csv_article_ID = null,pre_article_ID = null,product_temp = null;
-		String[] csv_product_ID = new String[50];
+		String article_ID = null,product_temp = null;
+		String csv_product_ID;
+		String[] csv_atricle_ID = new String[50];
         String str_readLine = "";
+        int count = 0;
 
-		/*以下原本為result黨的處理，不過目前應該用不著*/
-		if(file.exists())								
-			file.delete();
-        
         try {
 			fr = new FileReader("result.csv");
 			br = new BufferedReader(fr);
-			fw = new FileWriter(file,true);
 	
 			while (br.ready()) {
 				str_readLine = br.readLine();
-				csv_article_ID = str_readLine.split(":")[0];
-				if(pre_article_ID == csv_article_ID) {
-					product_temp = str_readLine.split(",")[1];
-
-					for(int j = 0;j<product_temp.split(" ").length;j++) {
-						csv_product_ID[j] = product_temp.split(" ")[j];
-						fw.write(csv_product_ID[j]+" ");				
+				article_ID = str_readLine.split(":")[0];
+				product_temp = str_readLine.split(",")[1];
+				
+				for(int i = 0;i<product_temp.split(" ").length;i++) {
+					csv_product_ID = product_temp.split(" ")[i];
+					if(search_str.compareTo(csv_product_ID) == 0) {
+						csv_atricle_ID[count] = article_ID;
+						count++;
 					}
-				}
-				else {
-					if(pre_article_ID != null && pre_article_ID != csv_article_ID)
-						fw.write("\n");
-					fw.write(csv_article_ID+",");
-					product_temp = str_readLine.split(",")[1];
-
-					for(int j = 0;j<product_temp.split(" ").length;j++) {
-						csv_product_ID[j] = product_temp.split(" ")[j];
-						fw.write(csv_product_ID[j]+" ");				
-					}
-					pre_article_ID = csv_article_ID;
 				}
 			}
+			/*
+			for(int j=0;j<csv_atricle_ID.length;j++) {
+				if(csv_atricle_ID[j] != null)
+					System.out.println(csv_atricle_ID[j]);
+			}
+			*/
 		} catch (FileNotFoundException e) {
 			// TODO 自動產生的 catch 區塊
 			e.printStackTrace();
 		}
-        /*result黨處理結束*/
+        return csv_atricle_ID;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new Main_Process().result_search("8RqztGxb2_8");
+		} catch (IOException e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		}
 	}
 }
